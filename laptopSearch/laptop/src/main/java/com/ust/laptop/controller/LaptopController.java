5d4f2af9ae7e4e3e62b562777cd9f44c476f4698
AAKASH NAIR <7akashnair@gmail.com>
2024-10-04 16:47:15 +0530

    Update LaptopController.java

--- laptopSearch/laptop/src/main/java/com/ust/laptop/controller/LaptopController.java
+++ laptopSearch/laptop/src/main/java/com/ust/laptop/controller/LaptopController.java
@@ -20,37 +20,37 @@ public class LaptopController {
     @Autowired
     private LaptopService service;
 
-    // Add a new laptop
+    
     @PostMapping("/add")
     public Laptopinfo addLaptop(@RequestBody Laptopinfo laptopinfo){
         return service.addlaptop(laptopinfo);
     }
 
-    // Get all laptops
+    
     @GetMapping("/all")
     public List<Laptopinfo> getAllLaptops(){
         return service.getlaptop();
     }
 
-    // Search laptops by price range
+    
     @GetMapping("/price")
     public List<Laptopinfo> getLaptopsByPrice(@RequestParam double minPrice, @RequestParam double maxPrice) {
         return service.findByPrice(minPrice, maxPrice);
     }
 
-    // Search laptops by brand
+    
     @GetMapping("/brand")
     public List<Laptopinfo> getLaptopsByBrand(@RequestParam String brand) {
         return service.findByBrand(brand);
     }
 
-    // Search laptops by processor
+    
     @GetMapping("/processor")
     public List<Laptopinfo> getLaptopsByProcessor(@RequestParam String processor) {
         return service.findByProcessor(processor);
     }
 
-    // Search laptops by storage capacity
+    
     @GetMapping("/storage")
     public List<Laptopinfo> getLaptopsByStorage(@RequestParam int storage) {
         return service.findByStorage(storage);
